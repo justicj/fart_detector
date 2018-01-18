0631f20e2dd740177a8e67802e8ba3d402a24e0b
Jason Justice <jayjustice@gmail.com>
2018-01-18 06:33:46 +0000

    final touches

--- fart_detector.py
+++ fart_detector.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import time, RPi.GPIO as GPIO
 from time import sleep
+import os
 trigger = 4
 fart = 0
 wait_time = 15
@@ -9,23 +10,35 @@ GPIO.setup(trigger, GPIO.IN)
 
 def detect_fart(trigger):
     fart = 0
+    clean_up = os.system("clear")
     print "Engaging fart detection"
+    sleep(0.75)
+    clean_up = os.system("clear")
+    print "Engaging fart detection."
+    sleep(0.75)
+    clean_up = os.system("clear")
+    print "Engaging fart detection.."
+    sleep(0.75)
+    clean_up = os.system("clear")
+    print "Engaging fart detection..."
     print "Actively monitoring for farts"
     while True:
       i = GPIO.input(trigger)
       if i == 1:
-        print "Fart Detected!"
+	print "Fart Detected!"
         fart = 1
+	sleep(3)
         break
       sleep(0.1)
     return fart
 
 def clear_the_air(wait_time):
     for i in range(wait_time):
-        time_left = wait_time - i
-        print "Waiting for " + str(time_left) + " seconds, for the air to clear!"
-        sleep(1)
-
+	time_left = wait_time - i
+        print 'Waiting ' + str(time_left) + ' seconds for the air to clear!'
+	sleep(1)
+	clean_up = os.system("clear")
+	print "Fart Detected!"
 while True:
     fart = detect_fart(trigger)
     if fart == 1:
