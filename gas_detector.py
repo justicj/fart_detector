052a591a37c6815c898d72414f8c381a4e508427
Jason Justice <jayjustice@gmail.com>
2018-01-28 18:57:24 -0800

    comments for led

--- gas_detector.py
+++ gas_detector.py
@@ -4,56 +4,58 @@ import time
 import RPi.GPIO as GPIO
 from time import sleep
 import os
+from pygame import mixer
 trigger = 4
-fart = 0
+gas = 0
 wait_time = 10
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(trigger, GPIO.IN)
 
 
-def detect_fart(trigger):
-    fart = 0
+def detect_gas(trigger):
+    gas = 0
     clean_up = os.system("clear")
-    print "Engaging fart detection"
+    print "Engaging gas detection"
     sleep(0.75)
     clean_up = os.system("clear")
-    print "Engaging fart detection."
+    print "Engaging gas detection."
     sleep(0.75)
     clean_up = os.system("clear")
-    print "Engaging fart detection.."
+    print "Engaging gas detection.."
     sleep(0.75)
     clean_up = os.system("clear")
-    print "Engaging fart detection..."
-    print "Actively monitoring for farts"
+    print "Engaging gas detection..."
+    print "Actively monitoring for gas"
     while True:
         i = GPIO.input(trigger)
         if i == 1:
-            print "Fart Detected!"
+            print "Gas Detected!"
 	    print "¯\_(ツ)_/¯"
-            fart = 1
+            gas = 1
             sleep(0)
             break
         sleep(0.1)
-    return fart
+    return gas
 
 
-def clear_the_air(wait_time):
-    from pygame import mixer    
+def clear_the_air(wait_time):  
     mixer.init()
     mixer.music.load("evacuate.mp3")
     for i in range(wait_time):
         time_left = wait_time - i
         print 'Waiting ' + str(time_left) + ' seconds for the air to clear!'
 	mixer.music.play(1)
+	#placeholder for led on
 	sleep(1)
+	#placeholder for led off
         clean_up = os.system("clear")
-        print "Fart Detected!"
+        print "Gas Detected!"
 	print "¯\_(ツ)_/¯"
     mixer.music.stop()
 
 while True:
-    fart = detect_fart(trigger)
-    if fart == 1:
+    gas = detect_gas(trigger)
+    if gas == 1:
         clear_the_air(wait_time)
 
 GPIO.cleanup()
